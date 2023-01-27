f6cc47dd8456a5edbe673ffd4bcde5c5aa9fc927
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-27 17:45:25 -0500

    added fetch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,33 @@
-let lat = "43.65";
-let lon = "-79.34";
-let city = "London";
+// let lat = "43.65";
+// let lon = "-79.34";
+// let city = "London";
 let apiKey = "91968567ff99e1beb7a3cbfb1666acb9";
-let requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
-let requestUrlcity = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
+// let requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
+// let requestUrlcity = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
 
 let searchBtn = document.getElementById("searchBtn");
-
-// fetch(requestUrlcity)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//   });
 
 searchBtn.addEventListener("click", getWeatherData);
 
 function getWeatherData(event){
   event.preventDefault();
-  console.log("hello");
+  let selectedCity = document.getElementById("city-input").value;
+  if (selectedCity===""){
+    window.alert("Please type a city name in the search input box");
+    return;
+  }
+  let requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${selectedCity}&appid=${apiKey}`;
+  
+  fetch(requestUrl)
+  .then(function (response) {
+    if (response.ok===false) {
+    alert("Please check the city name again. It may be incorrect.");
+    return;
+    } else {
+      return response.json();
+    } 
+  })
+  .then(function (data) {
+    console.log(data);
+  });
 }
