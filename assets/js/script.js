a092d2f257c05377f1bba8fd74a9236886bd57ae
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-26 21:43:31 -0500

    Added a fetch using a city name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 let lat = "43.65";
 let lon = "-79.34";
+let city = "Atlanta";
 let apiKey = "91968567ff99e1beb7a3cbfb1666acb9";
-requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
+requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
 
 console.log(requestUrl);
 
