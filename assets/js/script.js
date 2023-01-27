aeb59e2cf47ac79875b8805415be714a2e0622d0
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-26 22:52:32 -0500

    Added some Bootstrap templates to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,12 +5,12 @@ let apiKey = "91968567ff99e1beb7a3cbfb1666acb9";
 requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
 requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
 
-console.log(requestUrl);
+// console.log(requestUrl);
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+// fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
