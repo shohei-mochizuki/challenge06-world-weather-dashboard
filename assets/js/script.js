6eaf65130f6c4ca3fa7929497f217c5d77197ba1
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-01-27 16:57:51 -0500

    Updated HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 let lat = "43.65";
 let lon = "-79.34";
-let city = "Atlanta";
+let city = "London";
 let apiKey = "91968567ff99e1beb7a3cbfb1666acb9";
-requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
-requestUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
+let requestUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`; // Search with lat & lon
+let requestUrlcity = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Search with city name
 
-// console.log(requestUrl);
+let searchBtn = document.getElementById("searchBtn");
 
-// fetch(requestUrl)
+// fetch(requestUrlcity)
 //   .then(function (response) {
 //     return response.json();
 //   })
 //   .then(function (data) {
 //     console.log(data);
 //   });
+
+searchBtn.addEventListener("click", getWeatherData);
+
+function getWeatherData(event){
+  event.preventDefault();
+  console.log("hello");
+}
